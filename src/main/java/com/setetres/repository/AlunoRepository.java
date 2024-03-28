6304c8843e7d7ebedae82918bd540163f11a9f77
Leonardo Sigolo <leonardo.sigolo1037@gmail.com>
2024-03-28 15:33:38 -0300

    avaliacao2803

---
+++ src/main/java/com/setetres/repository/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.setetres.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.setetres.entities.Aluno;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long>{
+
+}
---
+++ src/main/java/com/setetres/repository/TurmaRepository.java
@@ -0,0 +1,9 @@
+package com.setetres.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.setetres.entities.Turma;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long>{
+
+}
